f817ae24519945850cf00d49fd6899237c2113ac
Suvrajit Ray <suvrajit@Suvrajits-Mac-mini.local>
2024-04-27 13:38:40 +0530

    keymap changes.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,25 +1,30 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local map = vim.keymap.set
+local keymap = vim.keymap
 
 -- better escape
-map("i", "jk", "<Esc>", { desc = "exit insert mode with jk", silent = true, noremap = true })
-map("i", "kj", "<Esc>", { desc = "exit insert mode with kj", silent = true, noremap = true })
+keymap.set("i", "jk", "<Esc>", { desc = "exit insert mode with jk", silent = true, noremap = true })
+keymap.set("i", "kj", "<Esc>", { desc = "exit insert mode with kj", silent = true, noremap = true })
 
 -- no highligh
-map("n", "<leader>nh", ":nohl<CR>", { desc = "clear search highlights", silent = true, noremap = true })
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "clear search highlights", silent = true, noremap = true })
 
 -- up and down and left and right navigation
-map({ "n", "x", "v" }, "<Up>", "5k", { desc = "move the cursor to five line up", silent = true, noremap = true })
-map({ "n", "x", "v" }, "<Down>", "5j", { desc = "move the cursor to five lines down", silent = true, noremap = true })
-map(
+keymap.set({ "n", "x", "v" }, "<Up>", "5k", { desc = "move the cursor to five line up", silent = true, noremap = true })
+keymap.set(
+  { "n", "x", "v" },
+  "<Down>",
+  "5j",
+  { desc = "move the cursor to five lines down", silent = true, noremap = true }
+)
+keymap.set(
   { "n", "x", "v" },
   "<Left>",
   "^",
   { desc = "move the cursor to first charactor of line", silent = true, noremap = true }
 )
-map(
+keymap.set(
   { "n", "x", "v" },
   "<Right>",
   "g_",
@@ -27,17 +32,31 @@ map(
 )
 
 -- paste over currently selected text without yanking it
-map("v", "p", '"_dp')
-map("v", "P", '"_dP')
+keymap.set("v", "p", '"_dp')
+keymap.set("v", "P", '"_dP')
 
 -- map ctrl + a to select all
-map("n", "<C-a>", "ggVG", { desc = "select all", silent = true, noremap = true })
+keymap.set("n", "<C-a>", "ggVG", { desc = "select all", silent = true, noremap = true })
 
 if not vim.g.vscode then
   -- redo.
-  map("n", "U", "<C-r>", { desc = "redo", silent = true, noremap = true })
+  keymap.set("n", "U", "<C-r>", { desc = "redo", silent = true, noremap = true })
 end
 
-map("n", "<CR>", 'vi"', { desc = "map enter to select all inside double quot", silent = true, noremap = true })
+keymap.set("n", "<CR>", 'vi"', { desc = "map enter to select all inside double quot", silent = true, noremap = true })
 
-map("n", "<BS>", "vi'", { desc = "map backspace to select all inside single quot", silent = true, noremap = true })
+keymap.set(
+  "n",
+  "<BS>",
+  "vi'",
+  { desc = "map backspace to select all inside single quot", silent = true, noremap = true }
+)
+
+keymap.set("n", "dw", 'vb"_d', { desc = "delete a word backwards", silent = true, noremap = true })
+
+keymap.set(
+  "n",
+  "x",
+  '"_x',
+  { desc = "delete a character without affecting the register", silent = true, noremap = true }
+)
--- lua/vscode/keymaps.lua
+++ lua/vscode/keymaps.lua
@@ -1,21 +1,22 @@
-local map = vim.keymap.set
+local keymap = vim.keymap
+local opts = { silent = true, noremap = true }
 
 -- map leader key
 -- vim.g.mapleader = " "
 
 -- navigation between windows.
-map("n", "<C-j>", ':call VSCodeNotify("workbench.action.navigateDown")<CR>', { silent = true, noremap = true })
-map("x", "<C-j>", ':call VSCodeNotify("workbench.action.navigateDown")<CR>', { silent = true, noremap = true })
-map("n", "<C-k>", ':call VSCodeNotify("workbench.action.navigateUp")<CR>', { silent = true, noremap = true })
-map("x", "<C-k>", ':call VSCodeNotify("workbench.action.navigateUp")<CR>', { silent = true, noremap = true })
-map("n", "<C-h>", ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', { silent = true, noremap = true })
-map("x", "<C-h>", ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', { silent = true, noremap = true })
-map("n", "<C-l>", ':call VSCodeNotify("workbench.action.navigateRight")<CR>', { silent = true, noremap = true })
-map("x", "<C-l>", ':call VSCodeNotify("workbench.action.navigateRight")<CR>', { silent = true, noremap = true })
+keymap.set("n", "<C-j>", ':call VSCodeNotify("workbench.action.navigateDown")<CR>', opts)
+keymap.set("x", "<C-j>", ':call VSCodeNotify("workbench.action.navigateDown")<CR>', opts)
+keymap.set("n", "<C-k>", ':call VSCodeNotify("workbench.action.navigateUp")<CR>', opts)
+keymap.set("x", "<C-k>", ':call VSCodeNotify("workbench.action.navigateUp")<CR>', opts)
+keymap.set("n", "<C-h>", ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', opts)
+keymap.set("x", "<C-h>", ':call VSCodeNotify("workbench.action.navigateLeft")<CR>', opts)
+keymap.set("n", "<C-l>", ':call VSCodeNotify("workbench.action.navigateRight")<CR>', opts)
+keymap.set("x", "<C-l>", ':call VSCodeNotify("workbench.action.navigateRight")<CR>', opts)
 
-map("n", "gr", ':call VSCodeNotify("editor.action.goToReferences")<CR>', { silent = true, noremap = true })
+keymap.set("n", "gr", ':call VSCodeNotify("editor.action.goToReferences")<CR>', opts)
 
 -- undo redo like vscode.
-map("n", "u", ':call VSCodeNotify("undo")<CR>', { silent = true, noremap = true })
-map("n", "U", ':call VSCodeNotify("redo")<CR>', { silent = true, noremap = true })
-map("n", "<C-r>", ':call VSCodeNotify("redo")<CR>', { silent = true, noremap = true })
+keymap.set("n", "u", ':call VSCodeNotify("undo")<CR>', opts)
+keymap.set("n", "U", ':call VSCodeNotify("redo")<CR>', opts)
+keymap.set("n", "<C-r>", ':call VSCodeNotify("redo")<CR>', opts)
