00f999008a1548a881b4d72554a7f9f445593164
Suvrajit Ray <suray@microsoft.com>
2024-05-03 19:28:44 +0530

    keymap changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,29 +11,30 @@ keymap.set("i", "kj", "<Esc>", { desc = "exit insert mode with kj", silent = tru
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "clear search highlights", silent = true, noremap = true })
 
 -- up and down and left and right navigation
-keymap.set({ "n", "x", "v" }, "<Up>", "5k", { desc = "move the cursor to five line up", silent = true, noremap = true })
-keymap.set(
-  { "n", "x", "v" },
-  "<Down>",
-  "5j",
-  { desc = "move the cursor to five lines down", silent = true, noremap = true }
-)
-keymap.set(
-  { "n", "x", "v" },
-  "<Left>",
-  "^",
-  { desc = "move the cursor to first charactor of line", silent = true, noremap = true }
-)
-keymap.set(
-  { "n", "x", "v" },
-  "<Right>",
-  "g_",
-  { desc = "move the cursor to last charactor of line", silent = true, noremap = true }
-)
+-- keymap.set({ "n", "x", "v" }, "<Up>", "5k", { desc = "move the cursor to five line up", silent = true, noremap = true })
+-- keymap.set(
+--   { "n", "x", "v" },
+--   "<Down>",
+--   "5j",
+--   { desc = "move the cursor to five lines down", silent = true, noremap = true }
+-- )
+-- keymap.set(
+--   { "n", "x", "v" },
+--   "<Left>",
+--   "^",
+--   { desc = "move the cursor to first charactor of line", silent = true, noremap = true }
+-- )
+-- keymap.set(
+--   { "n", "x", "v" },
+--   "<Right>",
+--   "g_",
+--   { desc = "move the cursor to last charactor of line", silent = true, noremap = true }
+-- )
 
 -- paste over currently selected text without yanking it
-keymap.set("v", "p", '"_dp')
-keymap.set("v", "P", '"_dP')
+
+keymap.set("v", "p", '"_dP')
+keymap.set("v", "P", '"_dp')
 
 -- map ctrl + a to select all
 keymap.set("n", "<C-a>", "ggVG", { desc = "select all", silent = true, noremap = true })
@@ -43,16 +44,11 @@ if not vim.g.vscode then
   keymap.set("n", "U", "<C-r>", { desc = "redo", silent = true, noremap = true })
 end
 
-keymap.set("n", "<CR>", 'vi"', { desc = "map enter to select all inside double quot", silent = true, noremap = true })
+keymap.set("n", "<BS>", 'vi"', { desc = "map backto select all inside single quot", silent = true, noremap = true })
 
-keymap.set(
-  "n",
-  "<BS>",
-  "vi'",
-  { desc = "map backspace to select all inside single quot", silent = true, noremap = true }
-)
+keymap.set("n", "<CR>", "vi'", { desc = "map enter to select all inside double quot", silent = true, noremap = true })
 
-keymap.set("n", "dw", 'vb"_d', { desc = "delete a word backwards", silent = true, noremap = true })
+-- keymap.set("n", "dw", 'vb"_d', { desc = "delete a word backwards", silent = true, noremap = true })
 
 keymap.set(
   "n",
